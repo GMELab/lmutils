c2a91bdd7345bbab02fa860aaec8f8d51e6fdf83
mrvillage <josef@mrvillage.dev>
2024-06-14 12:25:51 -0400

    Use capped parallelism for `calculate_r2s`

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ mod matrix;
 pub mod r;
 mod transform;
 
-use rayon::prelude::*;
+use std::sync::Mutex;
 
 pub use crate::{calc::*, errors::*, file::*, matrix::*, transform::*};
 
@@ -32,7 +32,9 @@ pub fn calculate_r2<'a>(
     outcomes: impl Transform<'a>,
 ) -> Result<Vec<R2>, ReadMatrixError> {
     let data = data.transform()?;
-    let outcomes = outcomes.transform()?;
+    let mut outcomes = outcomes.transform()?;
+    outcomes.remove_column_by_name_if_exists("eid");
+    outcomes.remove_column_by_name_if_exists("IID");
     let data = data.as_mat_ref()?;
     let outcomes = outcomes.as_mat_ref()?;
     Ok(get_r2s(data, outcomes))
@@ -45,36 +47,57 @@ pub fn calculate_r2s<'a>(
     data_names: Option<Vec<&str>>,
 ) -> Result<Vec<R2>, ReadMatrixError> {
     let mut outcomes = outcomes.transform()?;
+    outcomes.remove_column_by_name_if_exists("eid");
+    outcomes.remove_column_by_name_if_exists("IID");
     let colnames = outcomes
         .colnames()
         .map(|x| x.into_iter().map(|x| x.to_string()).collect::<Vec<_>>());
     let or = outcomes.as_mat_ref()?;
-    let data = data
-        .into_iter()
-        .map(|i| i.make_parallel_safe())
-        .collect::<Result<Vec<_>, _>>()?;
-    Ok(data
-        .into_par_iter()
-        .enumerate()
-        .map(|(i, data)| {
-            let mat = data.transform()?;
-            let r = mat.as_mat_ref()?;
-            Ok(get_r2s(r, or)
-                .into_iter()
-                .enumerate()
-                .map(|(j, mut r)| {
-                    if let Some(data_names) = &data_names {
-                        r.data = Some(data_names[i].to_string());
-                    }
-                    r.outcome = colnames
-                        .as_ref()
-                        .and_then(|c| c.get(j).map(|c| c.to_string()));
-                    r
-                })
-                .collect::<Vec<_>>())
-        })
-        .collect::<Result<Vec<_>, ReadMatrixError>>()?
-        .into_iter()
-        .flatten()
-        .collect())
+    let data = Mutex::new(
+        data.into_iter()
+            .enumerate()
+            .map(|(i, m)| m.make_parallel_safe().map(|m| (i, m)))
+            .collect::<Result<Vec<_>, _>>()?,
+    );
+    let results = Mutex::new(Vec::new());
+    let blocks_at_once = std::env::var("LMUTILS_BLOCKS_AT_ONCE")
+        .ok()
+        .and_then(|x| x.parse::<usize>().ok())
+        .unwrap_or(16)
+        .clamp(1, data.lock().unwrap().len());
+    std::thread::scope(|s| {
+        for _ in 0..blocks_at_once {
+            s.spawn(|| loop {
+                let mut guard = data.lock().unwrap();
+                let data = guard.pop();
+                drop(guard);
+                if let Some((i, data)) = data {
+                    rayon::scope(|s| {
+                        s.spawn(|_| {
+                            let mut mat = data.transform().unwrap();
+                            mat.remove_column_by_name_if_exists("eid");
+                            mat.remove_column_by_name_if_exists("IID");
+                            let r = mat.as_mat_ref().unwrap();
+                            let r2s = get_r2s(r, or)
+                                .into_iter()
+                                .enumerate()
+                                .map(|(j, mut r)| {
+                                    if let Some(data_names) = &data_names {
+                                        r.data = Some(data_names[i].to_string());
+                                    }
+                                    r.outcome = colnames
+                                        .as_ref()
+                                        .and_then(|c| c.get(j).map(|c| c.to_string()));
+                                    r
+                                })
+                                .collect::<Vec<_>>();
+                            results.lock().unwrap().extend(r2s);
+                        });
+                    });
+                }
+            });
+        }
+    });
+
+    Ok(results.into_inner().unwrap())
 }
