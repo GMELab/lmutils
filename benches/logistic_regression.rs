1995900fa1932c566d19bfc9b665347e621caafd
mrvillage <josef@mrvillage.dev>
2024-11-11 22:49:49 -0500

    Update benchmark

--- benches/logistic_regression.rs
+++ benches/logistic_regression.rs
@@ -1,42 +1,50 @@
+use std::fmt::Debug;
+
 use diol::prelude::*;
 use lmutils::{logistic_regression_irls, logistic_regression_newton_raphson};
 use rand_distr::Distribution;
 
+#[derive(Clone)]
+struct Arg {
+    len: usize,
+    xs:  Vec<f64>,
+    ys:  Vec<f64>,
+}
+
+impl Debug for Arg {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Arg").field("len", &self.len).finish()
+    }
+}
+
 fn main() -> std::io::Result<()> {
     let mut bench = Bench::new(BenchConfig::from_args());
-    bench.register_many(list![irls, newton_raphson], [5, 50, 500, 5000]);
+    let xs = statrs::distribution::Normal::new(0.0, 1.0).unwrap();
+    let ys = statrs::distribution::Bernoulli::new(0.5).unwrap();
+    let args = [5, 50, 500, 5000].iter().map(|len| {
+        let xs = xs
+            .sample_iter(rand::thread_rng())
+            .take(*len)
+            .collect::<Vec<_>>();
+        let ys = ys
+            .sample_iter(rand::thread_rng())
+            .take(*len)
+            .collect::<Vec<_>>();
+        Arg { len: *len, xs, ys }
+    });
+    bench.register_many(list![irls, newton_raphson], args);
     bench.run()?;
     Ok(())
 }
 
-fn irls(bencher: Bencher, len: usize) {
-    let xs = statrs::distribution::Normal::new(0.0, 1.0).unwrap();
-    let ys = statrs::distribution::Bernoulli::new(0.5).unwrap();
-    let xs = xs
-        .sample_iter(rand::thread_rng())
-        .take(len)
-        .collect::<Vec<_>>();
-    let ys = ys
-        .sample_iter(rand::thread_rng())
-        .take(len)
-        .collect::<Vec<_>>();
+fn irls(bencher: Bencher, Arg { len, xs, ys }: Arg) {
     let xs = faer::mat::from_column_major_slice(xs.as_slice(), len, 1);
     bencher.bench(|| {
         logistic_regression_irls(xs, &ys);
     });
 }
 
-fn newton_raphson(bencher: Bencher, len: usize) {
-    let xs = statrs::distribution::Normal::new(0.0, 1.0).unwrap();
-    let ys = statrs::distribution::Bernoulli::new(0.5).unwrap();
-    let xs = xs
-        .sample_iter(rand::thread_rng())
-        .take(len)
-        .collect::<Vec<_>>();
-    let ys = ys
-        .sample_iter(rand::thread_rng())
-        .take(len)
-        .collect::<Vec<_>>();
+fn newton_raphson(bencher: Bencher, Arg { len, xs, ys }: Arg) {
     let xs = faer::mat::from_column_major_slice(xs.as_slice(), len, 1);
     bencher.bench(|| {
         logistic_regression_newton_raphson(xs, &ys);
