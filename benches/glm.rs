7c21001c67a4ea2e6799739150329982001c634f
mrvillage <josef@mrvillage.dev>
2025-06-03 13:50:55 -0400

    Add r_firth to benchmark

--- benches/glm.rs
+++ benches/glm.rs
@@ -52,7 +52,8 @@ fn main() -> std::io::Result<()> {
             irls_seq,
             irls_firth,
             irls_firth_seq,
-            r
+            r,
+            r_firth,
         ],
         args,
     );
@@ -66,7 +67,7 @@ fn irls(bencher: Bencher, Arg { nrow, ncol, xs, ys }: Arg) {
         NonZero::new(rayon::current_num_threads()).unwrap(),
     ));
     bencher.bench(|| {
-        Glm::irls::<family::GaussianIdentity>(xs, &ys, 1e-8, 25, false);
+        Glm::irls::<family::BinomialLogit>(xs, &ys, 1e-8, 25, false);
     });
 }
 
@@ -74,7 +75,7 @@ fn irls_seq(bencher: Bencher, Arg { nrow, ncol, xs, ys }: Arg) {
     let xs = MatRef::from_column_major_slice(xs.as_slice(), nrow, ncol);
     set_global_parallelism(faer::Par::Seq);
     bencher.bench(|| {
-        Glm::irls::<family::GaussianIdentity>(xs, &ys, 1e-8, 25, false);
+        Glm::irls::<family::BinomialLogit>(xs, &ys, 1e-8, 25, false);
     });
 }
 
@@ -84,7 +85,7 @@ fn irls_firth(bencher: Bencher, Arg { nrow, ncol, xs, ys }: Arg) {
         NonZero::new(rayon::current_num_threads()).unwrap(),
     ));
     bencher.bench(|| {
-        Glm::irls::<family::GaussianIdentity>(xs, &ys, 1e-8, 25, true);
+        Glm::irls::<family::BinomialLogit>(xs, &ys, 1e-8, 25, true);
     });
 }
 
@@ -92,14 +93,14 @@ fn irls_firth_seq(bencher: Bencher, Arg { nrow, ncol, xs, ys }: Arg) {
     let xs = MatRef::from_column_major_slice(xs.as_slice(), nrow, ncol);
     set_global_parallelism(faer::Par::Seq);
     bencher.bench(|| {
-        Glm::irls::<family::GaussianIdentity>(xs, &ys, 1e-8, 25, true);
+        Glm::irls::<family::BinomialLogit>(xs, &ys, 1e-8, 25, true);
     });
 }
 
 fn _newton_raphson(bencher: Bencher, Arg { nrow, ncol, xs, ys }: Arg) {
     let xs = MatRef::from_column_major_slice(xs.as_slice(), nrow, ncol);
     bencher.bench(|| {
-        Glm::newton_raphson::<family::GaussianIdentity>(xs, &ys, 1e-8, 25);
+        Glm::newton_raphson::<family::BinomialLogit>(xs, &ys, 1e-8, 25);
     });
 }
 
@@ -118,7 +119,50 @@ fn r(bencher: Bencher, Arg { nrow, ncol, xs, ys }: Arg) {
             xs <- lmutils::load("glm.mat")[[1]]
             ys <- lmutils::load("glm_ys.mat")[[1]][,1]
             start <- Sys.time()
-            m <- glm(ys ~ xs, family = gaussian(link="identity"))
+            m <- glm(ys ~ xs, family = binomial(link="logit"))
+            elapsed <- Sys.time() - start
+            cat(as.character(as.numeric(elapsed, units = "secs"), digits = 22))
+            "#,
+    )
+    .unwrap();
+    let timings = (0..5)
+        .map(|_| {
+            let output = std::process::Command::new("Rscript")
+                .arg("glm.r")
+                .output()
+                .unwrap();
+            if !output.status.success() {
+                panic!("{}", String::from_utf8(output.stderr).unwrap());
+            }
+            let output = String::from_utf8(output.stdout).unwrap();
+            output.trim().parse::<f64>().unwrap()
+        })
+        .collect::<Vec<_>>();
+    let duration =
+        std::time::Duration::from_secs_f64(timings.iter().sum::<f64>() / timings.len() as f64);
+
+    bencher.bench(|| std::thread::sleep(duration));
+    std::fs::remove_file("glm.r").unwrap();
+    std::fs::remove_file("glm.mat").unwrap();
+    std::fs::remove_file("glm_ys.mat").unwrap();
+}
+
+fn r_firth(bencher: Bencher, Arg { nrow, ncol, xs, ys }: Arg) {
+    let mut mat = OwnedMatrix::new(nrow, ncol, xs, None).into_matrix();
+    File::new("glm.mat", lmutils::FileType::Mat, false)
+        .write(&mut mat)
+        .unwrap();
+    let mut mat = OwnedMatrix::new(nrow, 1, ys, None).into_matrix();
+    File::new("glm_ys.mat", lmutils::FileType::Mat, false)
+        .write(&mut mat)
+        .unwrap();
+    std::fs::write(
+        "glm.r",
+        r#"
+            xs <- lmutils::load("glm.mat")[[1]]
+            ys <- lmutils::load("glm_ys.mat")[[1]][,1]
+            start <- Sys.time()
+            m <- logistf::logistf(ys ~ xs)
             elapsed <- Sys.time() - start
             cat(as.character(as.numeric(elapsed, units = "secs"), digits = 22))
             "#,
